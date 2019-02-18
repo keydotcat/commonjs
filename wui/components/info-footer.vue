bb1f90f40731e83b5918e7379a540c33a8864f0f
Adria Casajus <adriancasajus@gmail.com>
2019-02-18 14:51:29 +0100

    new: Added footer with version and stuff

---
+++ wui/components/info-footer.vue
@@ -0,0 +1,14 @@
+<template>
+  <footer class="footer mt-auto py-2 bg-light">
+    <div class="d-flex mx-1">
+      <span class="text-muted ml-auto mr-3">{{ `s:${$store.state.public.serverVersion}|w:${$store.state.public.webVersion}` }}@<a href="https://key.cat" class="text-info">KeyCat</a></span>
+    </div>
+  </footer>
+</template>
+
+<script>
+export default {
+  name: 'info-footer',
+}
+
+</script>
