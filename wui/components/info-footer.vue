541a90ceaa0431306ed1d0155f614d262ab5d280
Adria Casajus <adriancasajus@gmail.com>
2019-02-18 15:25:53 +0100

    lint

--- wui/components/info-footer.vue
+++ wui/components/info-footer.vue
@@ -1,14 +1,15 @@
 <template>
   <footer class="footer mt-auto py-2 bg-light">
     <div class="d-flex mx-1">
-      <span class="text-muted ml-auto mr-3">{{ `s:${$store.state.public.serverVersion}|w:${$store.state.public.webVersion}` }}@<a href="https://key.cat" class="text-info">KeyCat</a></span>
+      <span class="text-muted ml-auto mr-3">
+        {{ `s:${$store.state.public.serverVersion}|w:${$store.state.public.webVersion}` }}@<a href="https://key.cat" class="text-info">KeyCat</a>
+      </span>
     </div>
   </footer>
 </template>
 
 <script>
 export default {
-  name: 'info-footer',
+  name: 'info-footer'
 }
-
 </script>
